1cf7cc56f3d60613580cf597a9faa98c4608d4cf
Olamide <olire96@gmail.com>
2023-07-26 18:29:27 +0100

    Task 3 and 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Implementation of MRU cache replacement policy"""
+
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Implementation of FIFO caching"""
+    def __init__(self):
+        """Transfers the init from the parent class"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Inserts item into the cache data"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.queue.remove(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            deleted_key = self.queue.pop(len(self.queue) - 1)
+            del self.cache_data[deleted_key]
+            print("DISCARD: {}".format(deleted_key))
+        self.queue.append(key)
+
+    def get(self, key):
+        """Gets items from the cache data"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.cache_data[key]
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -6,3 +6,5 @@ These are the modules in the directory:
 1) 0-basic_cache.py - Creates a class BasicCache that inherits from BaseCaching and is a caching system
 2) 1-fifo_cache.py - Implementation of FIFO cache replacement policy
 3) 2-lifo_cache.py - Implementation of LIFO cache replacement policy
+4) 3-lru_cache.py - Implementation of LRU cache replacement policy
+5) 4-mru_cache.py - Implementation of MRU cache replacement policy

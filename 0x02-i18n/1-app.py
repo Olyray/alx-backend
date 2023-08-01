200c0d5a9acd7c8a0bb573ddbf2593e6328fd822
Olamide <olire96@gmail.com>
2023-08-01 21:02:42 +0100

    The second task

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,14 +5,24 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
-
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """A flask babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
 
 
 @app.route('/')
 def index():
     """Renders the index template"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
--- 0x02-i18n/README.md
+++ 0x02-i18n/README.md
@@ -1,1 +1,6 @@
 The files and sub directories in this directory are the solutions for the tasks in the internationalisation project.
+
+0) 0-app.py - A simple flask app
+1) 1-app.py - Adds babel configuration to the flask app
+
+/templates - Contains html templates for the flask apps

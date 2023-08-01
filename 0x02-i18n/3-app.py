133073f1c89cdf5728cdbaa74269fa14db3be26c
Olamide <olire96@gmail.com>
2023-08-01 22:01:59 +0100

    The fourth task

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""A simple flask app"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """A flask babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale():
+    """Chooses the appropriate language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Renders the index template"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
